e6968a1a2e20f573ef82a94973fdd3d52037047f
aaron <gilmanaa2@gmail.com>
2019-08-31 14:12:33 +0300

    added simple image

--- green_sports_native/App.js
+++ green_sports_native/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={{width: 300, height: 300}}
+        source={require('./assets/icon.png')}
+      />
     </View>
   );
 }
